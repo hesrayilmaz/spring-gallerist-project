41ba59bd046977a5d70828b9f220a0e85d167f14
Esra YILMAZ <hesra.yilmazz@gmail.com>
2025-12-08 07:08:33 +0300

    exception structure created

---
+++ src/main/java/com/esrayilmaz/exception/BaseException.java
@@ -0,0 +1,9 @@
+package com.esrayilmaz.exception;
+
+
+public class BaseException extends RuntimeException {
+
+	public BaseException(ErrorMessage errorMessage) {
+		super(errorMessage.prepareErrorMessage());
+	}
+}
